34de7973026ce664273fdc36d8e45e8bab5dd7c6
Nikita Vasilev <vasnikserg@yandex.ru>
2021-02-27 11:22:36 +0300

    plan

--- video_service/server.go
+++ video_service/server.go
@@ -2,4 +2,13 @@ package video_service
 
 func main() {
 	// cм controller, который будет вызываться из http
+
+	// rtmp_server := rtmp.CreateRtmpServer(":1935")
+	// rtmp_server.Run()
+	// rtmp_to_hls := converter.CreateConverter(...)
+
+	// controller := controller.CreateController(rtmp_server, rtmp_to_hls)
+
+	// server := http.CreateServer(controller)
+	// server.ListenAndServe()
 }
