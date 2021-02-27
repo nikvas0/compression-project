5d760c00f37024b5dbf8a8b1af3d0f17f1687e9e
Nikita Vasilev <vasnikserg@yandex.ru>
2021-02-27 11:56:06 +0300

    fix

--- video_service/main.go
+++ video_service/main.go
@@ -1,12 +1,14 @@
-package video_service
+package main
 
-import "github.com/nikvas0/compression-project/video_service/rtmp"
+//import "github.com/nikvas0/compression-project/video_service/rtmp"
+import "log"
 
 func main() {
+	log.Println("kek")
 	// cм controller, который будет вызываться из http
 
-	rtmpServer := rtmp.CreateRtmpServer(":1935")
-	rtmpServer.Run()
+	//	rtmpServer := rtmp.CreateRtmpServer(":1935")
+	//rtmpServer.Run()
 	// rtmpToHls := converter.CreateConverter(...)
 
 	// controller := controller.CreateController(rtmp_server, rtmp_to_hls)
